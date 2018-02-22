a146defd3972f0fe9baadb21250b3fef005521b6
Leslie Kornes <lesliekornes@gmail.com>
2018-02-21 23:16:10 -0500

    Cleaned up stocks controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,23 +1,15 @@
 class StocksController < ApplicationController
 
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js { render partial: 'users/result' }
-        end
-      else
-        respond_to do |format|
-          flash.now[:danger] = "You have entered an incorrect symbol."
-          format.js { render partial: 'users/result' }
-        end
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = "You haven't entered a stock to search!"
     else
-      respond_to do |format|
-        flash.now[:danger] = "You haven't entered a stock to search!"
-        format.js { render partial: 'users/result' }
-      end
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "You have entered an incorrect symbol." unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
     end
   end
+  
 end
