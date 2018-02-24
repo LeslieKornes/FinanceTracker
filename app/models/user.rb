dbdab451fcc8f3c60d7a137acacb498a58b93c43
Leslie Kornes <lesliekornes@gmail.com>
2018-02-23 22:07:42 -0500

    Added method using previous two methods (correction: 2 commits ago, the method was to check if the stock existed in the user_stocks table) to check whether or not user can follow stock.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,12 @@ class User < ApplicationRecord
        user_stocks.where(stock_id: stock.id).exists?
      end
 
-     def stock_under_limit?
+     def under_stock_limit?
        (user_stocks.count < 10)
+     end
+
+     def can_add_stock?(ticker_symbol)
+       under_stock_limit? && !stock_already_added?(ticker_symbol)
      end
 
 end
